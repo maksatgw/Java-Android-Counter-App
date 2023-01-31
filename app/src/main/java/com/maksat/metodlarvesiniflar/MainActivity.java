77d98849032aba2187a6ba34a57fcccd4916073c
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2023-01-31 19:13:19 +0300

    first commit

---
+++ app/src/main/java/com/maksat/metodlarvesiniflar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.maksat.metodlarvesiniflar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("On create Called");
+
+    }
+
+    //counter app
+    int sayi = 0;
+    public void counter(View view){
+        TextView textView = findViewById(R.id.textView);
+        sayi++;
+        String s = String.valueOf(sayi);
+        textView.setText(s);
+    }
+
+    public void counterDec(View view){
+        TextView textView = findViewById(R.id.textView);
+        sayi--;
+        String s = String.valueOf(sayi);
+        textView.setText(s);
+    }
+}
